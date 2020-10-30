d3659c0c203f64c3b1aaccdf974944152e6a46a5
Claudio <claudio.jr@targetone.com.br>
2020-10-29 22:16:22 -0300

    Feita listagem, criação e exclusão de rep.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    api.post('repositories', { title: `Repositório ${Date.now()}`, url: 'https://www.github.com/ClaudioNJunior/teste', techs: ["NodeJS", "ReactJS"] }).then(response => {
+      setRepositories([...repositories, response.data]);
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete('repositories/' + id).then(response => {
+      const newRepositories = repositories.filter(repository => repository.id !== id);
+
+      setRepositories(newRepositories);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
